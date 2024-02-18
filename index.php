784dc2574a89141e88649f350c7deb0298e045fd
rezzcmd <ridwanarifin375@gmail.com>
2024-02-18 16:19:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
 <body>
     <h1>hellow</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque possimus atque eligendi inventore repellat consectetur consequuntur saepe cum tenetur! Assumenda mollitia explicabo ipsam qui est facere, iure tempore dolore expedita.</p>
+    <ul>
+        <li>
+            satu
+        </li>
+        <li>
+            dua
+        </li>
+    </ul>
 </body>
 </html>

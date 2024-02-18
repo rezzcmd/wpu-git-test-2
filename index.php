a520beca263f14cc9b6a9f179b513b864ea84b40
rezzcmd <ridwanarifin375@gmail.com>
2024-02-18 16:10:30 +0700

    menambahkan paragraft

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remot</title>
+</head>
+<body>
+    <h1>hellow</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque possimus atque eligendi inventore repellat consectetur consequuntur saepe cum tenetur! Assumenda mollitia explicabo ipsam qui est facere, iure tempore dolore expedita.</p>
+</body>
+</html>
